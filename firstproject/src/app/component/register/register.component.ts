19b38c0beaed648dd912f6a373160fc3f6f6c2f4
Sachet <rokadesachet@gmail.com>
2024-01-31 11:57:59 +0530

    Initial commit

---
+++ firstproject/src/app/component/register/register.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormvalidationService} from '../../service/formvalidation.service';
+import { NetworkcallService } from '../../service/networkcall.service';
+import { Studentregister } from '../../model/studentregister';
+import { subscribe } from 'diagnostics_channel';
+import { MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css',
+})
+export class RegisterComponent implements OnInit {
+  userRegistrationModel:Studentregister=new Studentregister();
+  constructor(public formvalidservice: FormvalidationService,
+    public urlservice:NetworkcallService,
+    public dialogreference:MatDialogRef<RegisterComponent>) {}
+  ngOnInit(): void {}
+
+
+submitdata(){
+  this.urlservice.createstudentreg(this.userRegistrationModel).subscribe(data=>{
+
+  });
+  this.onClose();
+  }
+onClose(){
+this.dialogreference.close()
+}
+
+}
---
+++ registrationpage/src/main/java/com/angularproject/registrationpage/Controller/controller.java
@@ -0,0 +1,25 @@
+package com.angularproject.registrationpage.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.angularproject.registrationpage.Model.registration;
+import com.angularproject.registrationpage.Service.studentservice;
+
+@RestController
+@RequestMapping("/student")
+public class controller  {
+    
+    @Autowired
+    studentservice studentserv;
+    @CrossOrigin(origins="http://localhost:4200", maxAge = 3600)
+    @PostMapping("/registration")
+    public registration  addData(@RequestBody registration registration){
+        return this.studentserv.addStudentService(registration);
+    }
+
+}
